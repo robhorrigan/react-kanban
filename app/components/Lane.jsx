13ce8bac0d02fceba5114a5e93c7b49185d01878
Robert Horrigan <roberthorrigan@gmail.com>
2016-04-05 14:17:51 -0500

    Use ESLint

---
+++ app/components/Lane.jsx
@@ -0,0 +1,59 @@
+import AltContainer from 'alt-container';
+import React from 'react';
+import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+import LaneActions from '../actions/LaneActions';
+
+export default class Lane extends React.Component {
+  render() {
+    const {lane, ...props} = this.props;
+
+    return (
+      <div {...props}>
+        <div className="lane-header">
+          <div className="lane-add-note">
+            <button onClick={this.addNote}>+</button>
+          </div>
+        </div>
+        <div className="lane-name">{lane.name}</div>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getNotesByIds().notes || []
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
+      </div>
+    );
+  }
+
+  editNote(id, task) {
+    if(!task.trim()) {
+      return;
+    }
+
+    NoteActions.update({id, task});
+  }
+
+  addNote = (e) => {
+    const laneId = this.props.lane.id;
+    const note = NoteActions.create({task: 'New Task'});
+
+    LaneActions.attachToLane({
+      noteId: note.id,
+      laneId
+    })
+  };
+
+  deleteNote = (noteId, e) => {
+    e.stopPropagation();
+
+    const laneId = this.props.lane.id;
+
+    LaneActions.detachFromLane({laneId, noteId});
+    NoteActions.delete(noteId);
+  }
+
+}
